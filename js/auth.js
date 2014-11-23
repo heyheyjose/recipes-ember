ac2d5bb3455a307c15de6969a99866277d0c7143
heyheyjose <heyheyjose@users.noreply.github.com>
2014-11-22 19:01:38 -0500

    auth is hooked up now

--- js/auth.js
+++ js/auth.js
@@ -1,32 +1,32 @@
 App.ApplicationRoute = Ember.Route.extend({
-  beforeModel: function (transition) {
-    var applicationController = this.controllerFor('application');
-    if (localStorage.authToken) {
-      this.controllerFor('application').login();
-    } else {
-      this.controllerFor('application').logout();
-    }
-  },
+	beforeModel: function (transition) {
+		var applicationController = this.controllerFor('application');
+		if (localStorage.authToken) {
+			this.controllerFor('application').login();
+		} else {
+			this.controllerFor('application').logout();
+		}
+	},
 
-  actions: {
-    logout: function () {
-      this.controllerFor('application').logout();
-      delete localStorage.authToken;
-      this.transitionTo('login');
-    }
-  }
+	actions: {
+		logout: function () {
+			this.controllerFor('application').logout();
+			delete localStorage.authToken;
+			this.transitionTo('login');
+		}
+	}
 });
 
 App.AuthenticatedRoute = Ember.Route.extend({
-  beforeModel: function (transition) {
-    var applicationController = this.controllerFor('application');
-    if (!localStorage.authToken) {
-      applicationController.set('savedTransition', transition);
-      this.transitionTo('login');
-    } else {
-      this.controllerFor('application').login();
-    }
-  }
+	beforeModel: function (transition) {
+		var applicationController = this.controllerFor('application');
+	    if (localStorage.authToken !== App.Firebase.getAuth().token) {
+	    	applicationController.set('savedTransition', transition);
+	    	this.transitionTo('login');
+	    } else {
+	    	this.controllerFor('application').login();
+	    }
+	}
 });
 
 App.LoginRoute = Ember.Route.extend({
